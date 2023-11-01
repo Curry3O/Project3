67a947669824bfe17460a9167228c2d7fc8b8c28
hebin29 <riverhe@shein.com>
2023-11-01 11:31:57 +0800

    feat: uat1

--- ReactTmall/models/business.js
+++ ReactTmall/models/business.js
@@ -1,27 +1,29 @@
-import {queryBusiness} from '@/services/business';
+import { queryBusiness } from '@/services/business';
 const BusinessModel = {
   namespace: 'business',
   state: {
-    businessData:[],
-    total:0,
+    businessData: [],
+    total: 0
   },
   effects: {
     *fetchLoadBusiness(_, { call, put }) {
       const response = yield call(queryBusiness, _.payload);
       yield put({
         type: 'changeBusinessData',
-        payload: response,
+        payload: response
       });
     }
   },
   reducers: {
     changeBusinessData(state, action) {
-      return { 
-      	...state,
-      	total:action.payload.data.total,
-      	businessData:action.payload.data.list
+      return {
+        ...state,
+        total: action.payload.data.total,
+        businessData: action.payload.data.list
       };
-    },
-  },
+    }
+  }
 };
 export default BusinessModel;
+
+// uat1
